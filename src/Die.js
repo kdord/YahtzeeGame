a961627aaf2faa1936c88264b9734ccb7b4d66bd
kdord <katya.koliada49@gmail.com>
2019-12-17 10:59:49 +0200

    add some styling to Component

--- src/Die.js
+++ src/Die.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
+import "./Die.css";
 
 class Die extends Component {
   render() {
     return (
-      <div>
-        <button
-          className={"Die"}
-          style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-          onClick={this.props.handleClick}
-        >
-          {this.props.value}
-        </button>
-      </div>
+      <button
+        className={"Die"}
+        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
+        onClick={this.props.handleClick}
+      >
+        {this.props.value}
+      </button>
     );
   }
 }
