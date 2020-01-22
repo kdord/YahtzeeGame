9b9713452c871d5d964817da1e9acdc211438656
kdord <katya.koliada49@gmail.com>
2020-01-22 14:22:27 +0200

    Updates

---
+++ precache-manifest.c9adaadc83eca0b184136e5e45e2533a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a39d82a7d5922c8c0f68d07ecc27539",
+    "url": "/YahtzeeGame/index.html"
+  },
+  {
+    "revision": "12304ea3f54fcde7cbca",
+    "url": "/YahtzeeGame/static/css/2.b4b8a3e0.chunk.css"
+  },
+  {
+    "revision": "2bf6642e80f47907e42d",
+    "url": "/YahtzeeGame/static/css/main.f581e4f1.chunk.css"
+  },
+  {
+    "revision": "12304ea3f54fcde7cbca",
+    "url": "/YahtzeeGame/static/js/2.6454d5fd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/YahtzeeGame/static/js/2.6454d5fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2bf6642e80f47907e42d",
+    "url": "/YahtzeeGame/static/js/main.6524cbfd.chunk.js"
+  },
+  {
+    "revision": "c997fc01bf6ac8fffcfc",
+    "url": "/YahtzeeGame/static/js/runtime-main.84c70d7b.js"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/YahtzeeGame/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/YahtzeeGame/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/YahtzeeGame/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/YahtzeeGame/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/YahtzeeGame/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/YahtzeeGame/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/YahtzeeGame/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/YahtzeeGame/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/YahtzeeGame/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/YahtzeeGame/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/YahtzeeGame/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/YahtzeeGame/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/YahtzeeGame/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/YahtzeeGame/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/YahtzeeGame/static/media/fa-solid-900.f6121be5.woff2"
+  }
+]);
